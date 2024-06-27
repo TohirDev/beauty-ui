8ce9c98f04e61f5f0c883e20ee0433d607b51cf7
TohirDev <isakulovtohir902@gmail.com>
2024-06-27 17:54:13 +0500

    Card components created: CardHeader, CardTitle, CardDesc, CardContent

--- src/Card.tsx
+++ src/Card.tsx
@@ -18,13 +18,10 @@ const StyledCard = styled.div<{
 
 const cardStyle: CSSProperties = {
   borderRadius: "0.5rem",
-  borderWidth: "1px",
-  borderStyle: "solid",
-  borderColor: "#d1d5db", // Adjust as necessary
-  backgroundColor: "#f8fafc", // Adjust as necessary
-  color: "#1f2937", // Adjust as necessary
+  border: "1px solid #E4E4E7",
+  backgroundColor: "#fff",
   boxShadow: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
-  width: "500px",
+  width: "fit-content",
   padding: "10px",
 };
 
@@ -36,7 +33,7 @@ export const Card: React.FC<CardProps> = ({ children, sx }) => {
 const cardHeaderStyles: CSSProperties = {
   display: "flex",
   flexDirection: "column",
-  padding: "1.5rem",
+  padding: "1rem",
   marginTop: "0.375rem", // 1.5 * 0.25rem = 0.375rem
 };
 
@@ -50,5 +47,65 @@ export const CardHeader: React.FC<CardProps> = ({ children, sx }) => {
   const combinedStyles = { ...cardHeaderStyles, ...(sx as CSSObject) };
   return (
     <SytledCardHeader sx={combinedStyles}>{children}</SytledCardHeader>
+  );
+};
+
+const StyledCardTitle = styled.h3<{
+  sx?: CSSProp;
+}>`
+  ${({ sx }) => sx && convertToCss(sx as CSSObject)}
+`;
+const cardTitleStyle: CSSObject = {
+  fontSize: "1.5rem",
+  fontWeight: 600,
+  lineHeight: "1",
+  letterSpacing: "-0.025em",
+};
+
+export const CardTitle: React.FC<CardProps> = ({ children, sx }) => {
+  return (
+    <StyledCardTitle sx={{ ...cardTitleStyle, ...(sx as CSSObject) }}>
+      {children}
+    </StyledCardTitle>
+  );
+};
+
+const cardDescriptionStyles: CSSObject = {
+  fontSize: "0.875rem",
+  color: "#898991",
+  marginTop: "6px",
+};
+
+const StyledCardDescription = styled.p<{
+  sx?: CSSProp;
+}>`
+  ${({ sx }) => sx && convertToCss(sx as CSSObject)}
+`;
+
+export const CardDescription: React.FC<CardProps> = ({ sx, children }) => {
+  return (
+    <StyledCardDescription
+      sx={{ ...cardDescriptionStyles, ...(sx as CSSObject) }}
+    >
+      {children}
+    </StyledCardDescription>
+  );
+};
+
+const StyledCardContent = styled.div<{
+  sx?: CSSProp;
+}>`
+  ${({ sx }) => sx && convertToCss(sx as CSSObject)}
+`;
+const paddingStyle: CSSObject = {
+  padding: "1.5rem", // p-6
+  paddingTop: "0", // pt-0
+};
+
+export const CardContent: React.FC<CardProps> = ({ sx, children }) => {
+  return (
+    <StyledCardContent sx={{ ...paddingStyle, ...(sx as CSSObject) }}>
+      {children}
+    </StyledCardContent>
   );
 };
