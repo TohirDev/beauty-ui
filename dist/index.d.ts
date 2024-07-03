1af3aa3672881b3efd89d327bedd4efbcdfc11c4
TohirDev <isakulovtohir902@gmail.com>
2024-07-03 15:19:58 +0500

    changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,7 @@
 export { Button } from "./Button";
 export { Typography } from "./Typography";
-export { Card } from "./Card";
-export { CardHeader } from "./Card";
-export { CardTitle } from "./Card";
-export { CardDescription } from "./Card";
-export { CardContent } from "./Card";
-export { CardFooter } from "./Card";
+export { Card, CardHeader, CardContent, CardDescription, CardFooter, CardTitle, } from "./Card";
 export { Input } from "./Input";
 export { Checkbox } from "./Checkbox";
 export { Label } from "./Label";
+export { Switch } from "./Switch";
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 export { Button } from "./Button";
 export { Typography } from "./Typography";
-export { Card } from "./Card";
-export { CardHeader } from "./Card";
-export { CardTitle } from "./Card";
-export { CardDescription } from "./Card";
-export { CardContent } from "./Card";
-export { CardFooter } from "./Card";
+export {
+  Card,
+  CardHeader,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardTitle,
+} from "./Card";
 export { Input } from "./Input";
 export { Checkbox } from "./Checkbox";
 export { Label } from "./Label";
+// export { HoverCard, HoverCardTrigger } from "./HoverCard";
+export { Switch } from "./Switch";
